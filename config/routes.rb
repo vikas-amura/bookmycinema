1ee4ad254c643cb5bf491e5122a545c3982ed7b9
chinmay-amuratech <chinmay@amuratech.com>
2016-01-13 18:22:50 +0530

    Devise mail configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'homes#index'
+  get 'display', to: 'homes#display'
+
   devise_for :users, controllers: { registrations: "users/registrations",  sessions: "users/sessions", passwords: "users/passwords", confirmations: "users/confirmations" }
 
   resources :theatres do
